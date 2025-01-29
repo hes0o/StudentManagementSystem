7a19b7d2855a04401789cc42bd1baef1a9b2b3ac
MUHAMMED ALKHATIB <mohammed12alkhatib@gmail.com>
2025-01-29 17:49:11 +0300

    me deleting everything mohammed did (:

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("here's the main class our work will start from here , i'll send you guys the updates on clickup");
+        System.out.println(
+                "here's the main class our work will start from here , i'll send you guys the updates on clickup");
         System.out.println(" hello");
+        System.out.println(10);
     }
 }
