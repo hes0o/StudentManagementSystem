34a648b7f32d872d1bf0eea40339f8db10e7d44f
Amir <amersheikhhader@gmail.com>
2025-04-29 02:38:27 +0300

    bla bla

--- src/Interfaces/DashBoard.java
+++ src/Interfaces/DashBoard.java
@@ -1,10 +1,16 @@
 package src.Interfaces;
 
-
+import javax.swing.*;
 
 public class DashBoard extends javax.swing.JFrame {
 
-
+    public DashBoard() {
+        setTitle("DashBoard");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
     public static void main(String[] args) {
         // Create instance of Dashboard to run
         new DashBoard();
--- src/Interfaces/LoginForm.java
+++ src/Interfaces/LoginForm.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.awt.*;
 
 public class LoginForm extends javax.swing.JFrame {
     private JTextField UsernameField;
